1a6b59983803284270a2c2d545f90dff929e8ab7
hsinghua <cowninda@gmail.com>
2020-01-16 23:28:16 +0800

    201taiwan_bank_money_rate

---
+++ 201taiwan_bank_rate.py
@@ -0,0 +1,28 @@
+#E2-1-1-1.py p2-6範例
+#抓取台銀匯率資料
+import requests 
+from bs4 import BeautifulSoup
+import csv
+from time import localtime,strftime, strptime, mktime
+from datetime import datetime
+from os.path import exists
+html = requests.get('https://rate.bot.com.tw/xrt?Lang=zh-TW')
+bsObj = BeautifulSoup(html.content,'lxml')
+for single_tr in bsObj.find('table',{'title':'牌告匯率'}).find('tbody').findAll('tr'):
+    cell = single_tr.findAll('td')
+    currency_name = cell[0].find('div',{'class':'visible-phone'}).contents[0]
+    currency_name = currency_name.replace('\r','')
+    currency_name = currency_name.replace('\n','')
+    currency_name = currency_name.replace(' ','')
+    currency_rate = cell[1].contents[0]#cell[1]是本行現金買入，cell[2]是本行現金賣出
+    print(currency_name,currency_rate)
+    file_name = 'E2-1-1-1'+currency_name+'.csv'
+    now_time = strftime('%Y-%m-%d %H:%M:%S',localtime())
+    if not exists(file_name):
+        data = [['時間','匯率'],[now_time,currency_rate]]
+    else:
+        data = [[now_time, currency_rate]]
+    f = open(file_name,'a')
+    w = csv.writer(f)
+    w.writerows(data)
+    f.close()
